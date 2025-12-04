ec3d7320ac26327816741be6208ea69cbc39ba22
Asad <asadatiq5@gmail.com>
2025-12-04 10:44:27 +0500

    Added Proposals

---
+++ cypress/e2e/Integration/Proposal/VerifyDocumentIsLoadingInProposal.js
@@ -0,0 +1,37 @@
+import Login from '../../../support/Login';
+import JobInformation from '../../../support/CustomerDetails/JobInformation';
+import ViewCustomerMenuOptions from '../../../support/ViewCustomerMenuOptions';
+import CustomerEstimates from '../../../support/CustomerEstimates';
+import Proposal from '../../../support/CustomerDetails/Proposal';
+
+describe('VerifyDocumentLoading', () => {
+  it('Verify Document is Loading in Proposal', { tags: 'Regression' }, () => {
+    const login = new Login();
+    const viewCustomerMenuOptions = new ViewCustomerMenuOptions();
+    const jobInformation = new JobInformation();
+    const customerestimates = new CustomerEstimates();
+    const proposal = new Proposal();
+
+    login.setLoginEmailAndPasswordUsingApi();
+    login.visitPage();
+
+    jobInformation.addNewJobFromFixture();
+    jobInformation.saveNewJob();
+
+    viewCustomerMenuOptions.viewEstimates();
+    customerestimates.estimateNameFromFixture();
+    customerestimates.productNameFromFixture();
+    customerestimates.saveEstimate();
+    customerestimates.newEstimateAssertion();
+
+    viewCustomerMenuOptions.viewProposals();
+    proposal.verifyPdfViewer();
+  });
+  Cypress.on(
+    'uncaught:exception',
+    (_err, _runnable) =>
+      // returning false here prevents Cypress from
+      // failing the test
+      false
+  );
+});
